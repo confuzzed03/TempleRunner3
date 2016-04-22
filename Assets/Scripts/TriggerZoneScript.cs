5369cd9721194f05330af1ff88cebe4cded8e46b
confuzzed03 <kim.4256@osu.edu>
2016-04-21 20:22:53 -0400

    Added snag powerups and fixed track

---
+++ Assets/Scripts/TriggerZoneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZoneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
